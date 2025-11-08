a24c73d512c51d253a3021cb833c191538af7504
Favas <72879643+buildwithfavas@users.noreply.github.com>
2025-11-09 02:13:27 +0530

    Vite + React + Typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container className="text-center mt-5">
+      <h1>Facial Recognition</h1>
+      <Button variant="primary" className="mt-3">
+        Start Detection
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
